6de18da391fbc29a07dd78474b154bb3ae106ab2
mafro <github@mafro.net>
2013-03-25 11:11:30 +1100

    minor updates to flask plugin

--- sesame/flask/script.py
+++ sesame/flask/script.py
@@ -10,13 +10,13 @@ manager = Manager(usage="Encrypt/decrypt Flask application config")
 @manager.option("-c", "--config",
                 help="Path to your Flask app config")
 @manager.option("-k", "--keyfile",
-                help="Path to keyczar encryption key")
+                help="Path to your keyczar encryption key")
 def encrypt(config, keyfile=None):
     "Encrypt a config file"
     try:
-        config = encrypt_config(config, keyfile)
-        if config is not None:
-            print "Application config encrypted at {0}".format(config)
+        conf_path = encrypt_config(config, keyfile)
+        if conf_path is not None:
+            print "Application config encrypted at {0}".format(conf_path)
 
     except ConfigError as e:
         sys.stderr.write("{0}\n".format(e))
@@ -24,15 +24,15 @@ def encrypt(config, keyfile=None):
 
 
 @manager.option("-c", "--config",
-                help="Path to your encrypted app config")
+                help="Path to your encrypted Flask app config")
 @manager.option("-k", "--keyfile",
-                help="Path to keyczar encryption key")
+                help="Path to your keyczar encryption key")
 def decrypt(config, keyfile):
     "Decrypt a config file"
     try:
-        config = decrypt_config(config, keyfile)
-        if config:
-            print "Application config decrypted at {0}".format(config)
+        conf_path = decrypt_config(config, keyfile)
+        if conf_path is not None:
+            print "Application config decrypted at {0}".format(conf_path)
 
     except ConfigError as e:
         sys.stderr.write("{0}\n".format(e))
