be20d5100532c53bc58e6bde088ea8f23ee3ce1b
mafro <github@mafro.net>
2013-09-13 08:45:04 +1000

    full rewrite to normalize CLI and improve validation

--- sesame/__init__.py
+++ sesame/__init__.py
@@ -3,8 +3,8 @@
 #
 
 __title__ = 'sesame'
-__version__ = '0.2.3'
-__build__ = 0x000023
+__version__ = '0.2.4'
+__build__ = 0x000024
 __author__ = 'Matt Black'
 __license__ = 'Simplified BSD License'
 __copyright__ = 'Copyright 2013 Matt Black'
