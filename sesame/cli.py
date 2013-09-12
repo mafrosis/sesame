b2c602af70bd60da301469877c81bd5633a3c226
mafro <github@mafro.net>
2013-09-12 13:44:54 +1000

    fixed force param; provide better output

--- sesame/cli.py
+++ sesame/cli.py
@@ -14,7 +14,7 @@ def entrypoint():
     pencrypt = subparsers.add_parser('encrypt',
         help='Encrypt a config file'
     )
-    pencrypt.set_defaults(func=encrypt_config, term='encrypted')
+    pencrypt.set_defaults(func=encrypt_config, term='encrypt')
 
     pencrypt.add_argument(
         '-c', '--config', action='store', required=True,
@@ -22,36 +22,38 @@ def entrypoint():
     pencrypt.add_argument(
         '-k', '--keyfile', action='store',
         help='Path to keyczar encryption key')
-    pencrypt.add_argument(
-        '-f', '--force', action='store',
-        help='Force creation of new encryption key')
 
     # parser for the options on decrypt
     pdecrypt = subparsers.add_parser('decrypt',
         help='Decrypt a config file'
     )
-    pdecrypt.set_defaults(func=decrypt_config, term='decrypted')
+    pdecrypt.set_defaults(func=decrypt_config, term='decrypt')
 
     pdecrypt.add_argument(
         '-c', '--config', action='store', required=True,
         help='Path to your encrypted app config')
     pdecrypt.add_argument(
-        '-k', '--keyfile', action='store', required=True,
+        '-k', '--keyfile', action='store',
         help='Path to your keyczar encryption key')
     pdecrypt.add_argument(
-        '-f', '--force', action='store',
+        '-f', '--force', action='store_true',
         help='Force overwrite of existing config file')
 
     args = parser.parse_args()
 
     try:
-        # call the relevant function
-        conf_path = args.func(args.config, args.keyfile, args.force)
-        if conf_path:
-            print 'Application config {0} at {1}'.format(args.term, conf_path)
+        # encrypt
+        if args.term == 'encrypt':
+            key_created = encrypt_config(args.config, args.keyfile)
+            print 'Application config encrypted at {0}.encrypted'.format(args.config)
 
-            if args.keyfile is None:
-                print 'Encryption key created at key.pem'
+            if key_created is True:
+                print 'Encryption key created at sesame.key'
+
+        # decrypt
+        else:
+            if decrypt_config(args.config, args.keyfile, args.force):
+                print 'Application config decrypted at {0}'.format(args.config)
 
     except ConfigError as e:
         sys.stderr.write('{0}\n'.format(e))
