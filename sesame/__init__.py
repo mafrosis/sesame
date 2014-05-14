cbfbd0aba3f7167298e0614edcb3b67d577bead6
mafro <github@mafro.net>
2014-05-14 15:41:34 +1000

    v0.3.3 release

--- sesame/__init__.py
+++ sesame/__init__.py
@@ -3,8 +3,8 @@
 #
 
 __title__ = 'sesame'
-__version__ = '0.3.2'
-__build__ = 0x000032
+__version__ = '0.3.3'
+__build__ = 0x000033
 __author__ = 'Matt Black'
 __license__ = 'Simplified BSD License'
 __copyright__ = 'Copyright 2014 Matt Black'
